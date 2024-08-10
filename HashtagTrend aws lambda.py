0109100ce7aa4024cd41cb7e8b0330719e8dccb7
ManishDeva98 <manishveni28@gmail.com>
2024-08-10 17:34:39 +0530

    Update HashtagTrend aws lambda.py

--- HashtagTrend aws lambda.py
+++ HashtagTrend aws lambda.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 def lambda_handler(event, context):
     session = boto3.Session(
-        aws_access_key_id='AKIA**************S',
-        aws_secret_access_key='Hi5K***************U',
+        aws_access_key_id='**********************',
+        aws_secret_access_key='**************************',
         region_name='eu-north-1'
     )
 
     dynamodb = session.resource('dynamodb')
-    table = dynamodb.Table('Posts')
-    post_id = event['post_id']
-    content = event['content']
-    hashtags = event['hashtags']
+    table = dynamodb.Table('Posts1')
+    post_id = event.get('post_id')
+    content = event.get('content')
+    hashtags = event.get('hashtags')
     timestamp = datetime.utcnow().isoformat()
     
     item = {
@@ -21,11 +21,20 @@ def lambda_handler(event, context):
         'Content': content,
         'Hashtags': hashtags,
         'Timestamp': timestamp
-    } 
-    
-    table.put_item(Item=item)
-    
+    }  
+    '''return {
+        'statusCode': 200,
+        'body': json.dumps(f'Data successfully written to DynamoDB : {dict(item)}')
+    }'''
+    try:
+        table.put_item(Item=item)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error writing to DynamoDB: {str(e)}')
+        }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Post saved successfully!')
+        'body': json.dumps('Data successfully written to DynamoDB')
     }
